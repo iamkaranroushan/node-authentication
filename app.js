79966921d74cf8c746af41bf82190bf00926170b
Karan Roushan <karan.122roushan@gmail.com>
2023-09-22 01:24:37 +0530

    added css styling to login page and sign up page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const expressLayout = require('express-ejs-layouts')
 const app = express()
 
 //middlewares
-app.use(express.static('/public'));
+app.use(express.static('public'));
 app.use(expressLayout)
 app.set('layout', './layouts/main')
 app.set('view engine', 'ejs');
